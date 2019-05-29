70aa6e14f10cdf74446cec308de386dd7d206ae3
Charlie Aguirre <cj.wast@hotmail.com>
2019-05-28 21:58:12 -0500

    before we leave

---
+++ script/objects.js
@@ -0,0 +1,8 @@
+class Snake{
+  constructor (){
+
+  }
+  keepMoving(){
+
+  }
+}
--- script/snake.js
+++ script/snake.js
@@ -1,7 +1,23 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 canvas.width = 100;
 canvas.height = 100;
+ctx.fillStyle = "#FFFF00";
+ctx.fillRect(0, 0, 100, 100);
 
-ctx.fillRect()
+// document.addEventListener("keydown", (event) => {
+
+//   console.log(event.keyCode, ' - ', event.key);
+// });
+
+
+var map = {}; // You could also use an array
+onkeydown = onkeyup = function (e) {
+  e = e || event; // to deal with IE
+  map[e.keyCode] = e.type == 'keydown';
+  console.table(map);
+
+
+
+}
